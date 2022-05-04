3ab64d254e6e0b70df12e2e527dea016581696dd
DESKTOP-LE1E270\pc <thien0101vn@gmail.com>
2022-05-05 01:33:50 +0700

    3.0

--- giohang.js
+++ giohang.js
@@ -1,11 +1,14 @@
 function loadgiohang() {
     let giohang = JSON.parse(localStorage.getItem('giohangcuatoi'));
     $("#giohang").empty();
+    let promises = [];
     giohang.forEach((movie, i) => {
-            axios.get(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=a7b3c9975791294647265c71224a88ad`).then(
-                (res) => {
-                    $("#giohang").append(
-                        `<div id=ctgiohang>
+        let p = axios.get(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=a7b3c9975791294647265c71224a88ad`);
+        promises.push(p);
+        p.then(
+            (res) => {
+                $("#giohang").append(
+                    `<div id=ctgiohang>
                         <div id="ttsp" >
                         <div> <img src="https://image.tmdb.org/t/p/original/${res.data.poster_path}" id="anhsp"></div>
                         <div id="tensp">${movie.ten}</div>
@@ -15,16 +18,25 @@ function loadgiohang() {
                         </div>
                         <div id= "slsp" >
                         <input type = "number" value=${movie.soluong} onchange="onchang('${movie.ten}',this.value)">  
-                        <button onclick=remove(${i  })>xoa</button> 
+                        <button onclick=remove(${i})>xoa</button> 
                         </div>
                     </div>
                    `)
-                },
-                (error) => { console.error }
-            )
-        }
-
-    )
+                $("#tinhtien").empty();
+                $("#tinhtien").append(`<div id="thanhtoan">
+                   <div id="td">
+                       <h3>tổng tiền thanh toán :</h3>
+                   </div>
+                   <div><input id="tongtien" type="number"   disabled />  </div>
+               </div>`);
+                // /tongtien();
+            },
+            (error) => { console.error }
+        )
+    });
+    Promise.all(promises).then((values) => {
+        tongtien();
+    });
     localStorage.setItem('giohangcuatoi', JSON.stringify(giohang));
     console.log('gio hang hien tai', giohang);
 
@@ -34,9 +46,11 @@ function remove(vitricanxoa) {
     let giohang = JSON.parse(localStorage.getItem("giohangcuatoi"))
     giohang.forEach((movie) => {
         if (giohang.id === movie.id) {
-            giohang.splice(vitricanxoa, 1);
+            let i = movie[id]
+
         }
     })
+    giohang.splice(vitricanxoa, 1);
 
     $("#giohang").empty();
     giohang.forEach((movie, i) => {
@@ -55,8 +69,15 @@ function remove(vitricanxoa) {
                         <input type = "number" value=${movie.soluong} onchange="onchang('${movie.ten}',this.value)">  
                         <button onclick=remove(${i})>xoa</button> 
                         </div>
-                    </div>
-                   `)
+                    </div>`)
+                    $("#tinhtien").empty();
+                    $("#tinhtien").append(`<div id="thanhtoan">
+                   <div id="td">
+                       <h3>tổng tiền thanh toán :</h3>
+                   </div>
+                   <div><input id="tongtien" type="number"  disabled/></div>
+               </div>`);
+                    tongtien();
                 },
                 (error) => { console.error }
             )
@@ -81,7 +102,8 @@ function tongtien() {
     let giohang = JSON.parse(localStorage.getItem("giohangcuatoi"))
     giohang.forEach((movie) => {
         tong += (movie.Giatien * 1 * movie.soluong * 1);
-    })
+    });
     localStorage.setItem('giohangcuatoi', JSON.stringify(giohang));
     document.querySelector('input[id="tongtien"]').value = tong;
 }
+//$(document).ready(tongtien);
